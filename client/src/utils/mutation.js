33ca71514a36671dfbf99a6fed84bbe221e16fa0
brendanjlewis <brendan.j.f.lewis@gmail.com>
2022-01-31 23:20:52 -0500

    added mutations for add_user, save_book, remove_book to mutation.js

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -11,3 +11,51 @@ export const LOGIN_USER = gql `
         }
     }
 `;
+
+export const ADD_USER = gql `
+    mutation addUser($username: String!, $password: String!, $email: String!) {
+        addUser(username: $username, password: $password, email: $email) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: SavedBookInput) {
+        saveBook(input: $input) {
+            username
+            _id
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+    }
+`;
