774a692e1a2201c41b8633f6fd36186078b644fb
brendanjlewis <brendan.j.f.lewis@gmail.com>
2022-01-31 22:38:30 -0500

    added login_user mutation to front end and imported to loginform.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,14 +1,17 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
+// import user mutation
+import { userMutation } from '@apollo/client';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
+// replaced login() with LOGIN_USER mutation functionality
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,18 +29,13 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const response = await login({
+        variables: { ...formState },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
     }
 
     setUserFormData({
